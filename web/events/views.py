aab5b761f88f6493f494fb23834a3d3f15b99254
Juan Ignacio Rodríguez de León <euribates@gmail.com>
2018-07-15 18:46:15 +0100

    - Footer in a separated file, footer.html; base.html includes it

--- web/events/views.py
+++ web/events/views.py
@@ -2,7 +2,9 @@
 # -*- coding: utf-8 -*-
 
 from django.shortcuts import render
+from django.conf import settings
 from . import models
+
 
 def index(request):
     events = models.Event.objects.filter(active=True)
@@ -11,26 +13,42 @@ def index(request):
         return render(request, 'events/no-events.html')
     if num_events == 1:
         event = events.first()
-        ticket_types = event.ticket_types.all().order_by('release_at')
-        num_options = ticket_types.count()
-        return render(request, 'events/event.html', {
-            'event': event,
-            'ticket_types': ticket_types,
-            })
+        return _view_event(request, event)
     else:
         return render(request, 'events/list-events.html', {
             'events': events.all()
             })
 
+
 def detail_event(request, slug):
-    from django.http import HttpResponse
-    return HttpResponse('ok' + slug)
+    event = models.Event.objects.get(slug=slug)
+    return _view_event(request, event)
+
+
+def _view_event(request, event):
+    ticket_types = event.ticket_types.all().order_by('release_at')
+    num_options = ticket_types.count()
+    return render(request, 'events/event.html', {
+        'event': event,
+        'ticket_types': ticket_types,
+        'num_options': num_options,
+        })
 
 
 def buy_ticket(request, id_ticket_type):
-    ticket_type = models.TicketType.objects.select_related('event').get(pk=id_ticket_type)
+    ticket_type = models.TicketType.objects  \
+        .select_related('event')  \
+        .get(pk=id_ticket_type)
     event = ticket_type.event
-    return render(request, 'events/buy_ticket.html', {
-        'event': event,
-        'ticket_type': ticket_type,
-        })
+    if request.method == 'POST':
+        from django.http import HttpResponse
+        buff = []
+        for k in request.POST:
+            buff.append('{}: {}'.format(k, request.POST[k]))
+        return HttpResponse('\n'.join(buff))
+    else:
+        return render(request, 'events/buy_ticket.html', {
+            'event': event,
+            'ticket_type': ticket_type,
+            'stripe_public_key': settings.STRIPE_PUBLIC_KEY,
+            })
