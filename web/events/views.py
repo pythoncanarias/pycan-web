d59e51102ff873535e96e71844fe099c83be918b
Juan Ignacio Rodríguez de León <euribates@gmail.com>
2018-07-15 22:18:37 +0100

    It works! Payments accepted

--- web/events/views.py
+++ web/events/views.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.conf import settings
+from django.urls import reverse
+
+import stripe
+
 from . import models
 
 
@@ -41,14 +45,59 @@ def buy_ticket(request, id_ticket_type):
         .get(pk=id_ticket_type)
     event = ticket_type.event
     if request.method == 'POST':
-        from django.http import HttpResponse
-        buff = []
-        for k in request.POST:
-            buff.append('{}: {}'.format(k, request.POST[k]))
-        return HttpResponse('\n'.join(buff))
+        email = request.POST['stripeEmail']
+        name = request.POST['name']
+        surname = request.POST['surname']
+        phone = request.POST.get('phone', None)
+        token = request.POST['stripeToken']
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        customer = stripe.Customer.create(
+            email=email,
+            source=token,
+            description='{}, {}'.format(surname, name),
+            )
+        charge = stripe.Charge.create(
+            customer=customer.id,
+            amount=ticket_type.price_in_cents,
+            currency='EUR',
+            description='{} for {}, {}'.format(
+                ticket_type.name,
+                surname,
+                name,
+                )
+            )
+        if charge.paid:
+            ticket = models.Ticket(
+                ticket_type=ticket_type,
+                number=ticket_type.next_number(),
+                name=name,
+                surname=surname,
+                email=email,
+                phone=phone,
+                )
+            ticket.save()
+            url = reverse('events:ticket_bought', args=(ticket.pk,))
+            return redirect(url)
+        else:
+            # Must implemente a proper response
+            from django.http import HttpResponse
+            return HttpResponse(
+                'Nope. Algo fallo en el pago: charge:{}'.format(charge)
+                )
     else:
         return render(request, 'events/buy_ticket.html', {
             'event': event,
             'ticket_type': ticket_type,
             'stripe_public_key': settings.STRIPE_PUBLIC_KEY,
             })
+
+
+def ticket_bought(request, id_ticket):
+    ticket = models.Ticket.objects.get(pk=id_ticket)
+    ticket_type = ticket.ticket_type
+    event = ticket_type.event
+    return render(request, 'events/ticket_bought.html', {
+        'ticket': ticket,
+        'ticket_type': ticket_type,
+        'event': event,
+        })
