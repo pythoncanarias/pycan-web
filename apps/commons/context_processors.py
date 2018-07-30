29b550bee251b412d0b79ee90c99106195bff410
Carlos Sosa <sosahcarlos@gmail.com>
2018-07-30 17:15:05 +0100

    Refactor assets management

--- apps/commons/context_processors.py
+++ apps/commons/context_processors.py
@@ -2,20 +2,8 @@ import os
 from django.conf import settings
 import json
 
-
-APPS = ("commons", "homepage", "events")
-
-
 def glob(request):
-    context = {"assets": {}}
-    for app in APPS:
-        manifest_path = os.path.join(
-            settings.STATICFILES_DIRS[0], app, "rev-manifest.json")
-        with open(manifest_path) as f:
-            manifest_json = json.load(f)
-        for k, v in manifest_json.items():
-            key = os.path.join(app, k)
-            value = os.path.join(settings.STATIC_URL, app, v)
-            context["assets"][key] = value
-
-    return context
+    manifest_path = os.path.join(settings.STATICFILES_DIRS[0], "rev-manifest.json")
+    with open(manifest_path) as f:
+        assets = json.load(f)
+    return {"assets": assets}
