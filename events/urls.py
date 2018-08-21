6179f07d502d296aa448e1611a70854c584e07ed
Sergio Delgado Quintero <sdelquin@gmail.com>
2018-08-21 13:30:21 +0100

    Fix regex url for CoC Fix typos in CoC text

--- events/urls.py
+++ events/urls.py
@@ -5,7 +5,7 @@ app_name = 'events'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    re_path(r'coc/(?P<language>\w+/)?', views.coc, name='coc'),
+    re_path(r'coc/((?P<language>\w+)/)?', views.coc, name='coc'),
     path('buy/<int:id_ticket_type>/', views.buy_ticket, name='buy_ticket'),
     path('<slug:slug1>/<slug:slug2>/', views.detail_event, name='detail_event'),
     path('ticket/<uuid:keycode>/', views.ticket_bought, name='ticket_bought'),
