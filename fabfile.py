699dad6ba3915286b2ec94dab5705fb447f0b7db
Sergio Delgado Quintero <sdelquin@gmail.com>
2018-08-21 10:29:13 +0100

    Add migrations to fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,14 +1,15 @@
 from fabric.api import env, local, cd, run
 
-env.hosts = ["pythoncanarias.es"]
+env.hosts = ['pythoncanarias.es']
 
 
 def deploy():
-    local("git push")
-    with cd("~/web"):
-        run("git pull")
-        run("pipenv install")
-        run("npm install --no-save")
-        run("gulp")
-        run("pipenv run python manage.py collectstatic --noinput --clear")
-        run("supervisorctl restart web")
+    local('git push')
+    with cd('~/web'):
+        run('git pull')
+        run('pipenv install')
+        run('npm install --no-save')
+        run('gulp')
+        run('pipenv run python manage.py migrate')
+        run('pipenv run python manage.py collectstatic --noinput --clear')
+        run('supervisorctl restart web')
