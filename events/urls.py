25310f28957539d7945d10e7dcebebd1f82fe168
Sergio Delgado Quintero <sdelquin@gmail.com>
2018-08-24 18:19:38 +0100

    Refactor CoC view

--- events/urls.py
+++ events/urls.py
@@ -1,11 +1,12 @@
-from django.urls import path, re_path
+from django.urls import path
 from . import views
 
 app_name = 'events'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    re_path(r'^coc/((?P<language>\w+)/)?$', views.coc, name='coc'),
+    path('coc/', views.coc, name='coc'),
+    path('coc/<language>/', views.coc, name='coc'),
     path('buy/<int:id_ticket_type>/', views.buy_ticket, name='buy_ticket'),
     path('<slug:slug1>/<slug:slug2>/', views.detail_event, name='detail_event'),
     path('ticket/<uuid:keycode>/', views.ticket_bought, name='ticket_bought'),
