de46f4c1032a84c8470633fd6078d9dc4f856b7e
Sergio Delgado Quintero <sdelquin@gmail.com>
2018-08-30 18:37:30 +0100

    Topic display event (#39)
    * Add required properties to models in order to display correctly an event
    
    * Refactor some properties on models
    
    * Use quotes when referencing foreign keys outside proper app
    
    * Refactor methods in models
    
    * Add method to get scheduled items for display
    
    * Convert properties in methods in case
    
    * Allow multiple speakers in an unique temporal-spaced scheduled item
    
    * Use real data on the event page
    
    * Add event hero image
    
    * Remove unnecessary code and files
    
    * Change the filename of default event hero photo
    
    * Fix str of Contact

--- speakers/models.py
+++ speakers/models.py
@@ -50,7 +50,7 @@ class Contact(models.Model):
     identifier = models.CharField(max_length=128)
 
     def __str__(self):
-        return self.href()
+        return self.href
 
     @property
     def href(self):
