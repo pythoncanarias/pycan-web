b6e332c4e2a69b60a01f54c85a8b8450dad31e3d
Juan Ignacio Rodríguez de León <euribates@gmail.com>
2018-09-30 22:31:48 +0100

    Enlaza a eventos solo si hay algún evento activo (#95)

--- homepage/views.py
+++ homepage/views.py
@@ -1,5 +1,10 @@
+#!/usr/bin/env python
+
 from django.shortcuts import render
+from events.models import Event
 
 
 def homepage(request):
-    return render(request, 'homepage/index.html')
+    return render(request, 'homepage/index.html', {
+        'active_events': Event.objects.all().filter(active=True).count(),
+        })
