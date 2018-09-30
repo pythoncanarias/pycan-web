355fe00b51c96d54583960961a166ce5249090f1
Juan Ignacio Rodriguez de Leon <euribates@gmail.com>
2018-09-30 22:21:34 +0100

    Enlaza a eventos solo si hay algún evento activo

--- commons/context_processors.py
+++ commons/context_processors.py
@@ -4,10 +4,7 @@ import json
 
 
 def glob(request):
-    manifest_path = os.path.join(
-        settings.STATICFILES_DIRS[0],
-        "rev-manifest.json"
-    )
+    manifest_path = os.path.join(settings.STATIC_ROOT, "rev-manifest.json")
     with open(manifest_path) as f:
         assets = json.load(f)
     return {"assets": assets}
