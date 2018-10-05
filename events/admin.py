533738b96b8f74f29229b9ea7812b60170bfd4f0
Juan Ignacio Rodríguez de León <euribates@gmail.com>
2018-10-05 16:41:00 +0100

    Hotfix#1 (#116)
    * Mejorar panel administrativo de entradas #111
    
    * No mpostramos los id. numericos de las entradas
    ni los keycode en nunguna url

--- events/admin.py
+++ events/admin.py
@@ -3,8 +3,11 @@ from django.contrib import admin
 from .models import Event
 
 
+
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name', ), }
     list_display = ('name', 'slug', 'active',
                     'opened_ticket_sales', 'start_date')
+
+
--- events/test_links.py
+++ events/test_links.py
@@ -10,6 +10,14 @@ def test_ticket_purchase_needs_an_integer():
     with pytest.raises(TypeError):
         links.ticket_purchase(None)
 
+def test_ticket_purchase():
+    assert links.ticket_purchase(9) == '/events/ticket/purchase/9/'
+
+
+def test_article_bought():
+    assert links.article_bought(9) == '/events/ticket/purchase/bought/9/'
+
+
 if __name__ == '__main__':
     pytest.main()
 
