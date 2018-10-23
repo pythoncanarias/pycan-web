a8e108ca50bac7fe9101a3c87ac843c9bb57e651
Sergio Delgado Quintero <sdelquin@gmail.com>
2018-10-23 18:37:55 +0100

    Finish API urls with slash

--- api/urls.py
+++ api/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'api'
 
 urlpatterns = [
-    path('v1/status', views.status, name='status'),
-    path('v1/events', views.list_events, name='events'),
-    path('v1/events/<slug>', views.detail_event, name='detail_event'),
+    path('v1/status/', views.status, name='status'),
+    path('v1/events/', views.list_events, name='events'),
+    path('v1/events/<slug>/', views.detail_event, name='detail_event'),
 ]
