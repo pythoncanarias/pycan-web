395477dd50a4514cff29c6e181f3b56068048ed6
Raul Marrero <raul.marrero@system73.com>
2018-11-05 14:22:59 +0000

    Add admin action and set the correct dpi for printable PDF

--- events/admin.py
+++ events/admin.py
@@ -1,6 +1,15 @@
-from django.contrib import admin
-
+from django.contrib import admin, messages
 from .models import Event, Badge
+
+
+def render_event_badges(modeladmin, request, queryset):
+    prints = []
+    for event in queryset:
+        prints.append(request.get_host() + event.render_all_badges())
+    messages.add_message(request, messages.INFO, f"Generated PDFs in -> {' '.join(prints)} ")
+
+
+render_event_badges.short_description = "Generate a PDF with all the badges"
 
 
 class BadgeInline(admin.StackedInline):
@@ -13,6 +22,7 @@ class BadgeInline(admin.StackedInline):
 class EventAdmin(admin.ModelAdmin):
     inlines = [BadgeInline]
     prepopulated_fields = {'slug': ('name', ), }
+    actions = [render_event_badges]
     list_display = ('name', 'slug', 'active',
                     'opened_ticket_sales', 'start_date')
 
