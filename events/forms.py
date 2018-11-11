ed5e6dd6d396dd2fbd3ec512287e0d9eeb7b604f
Juan Ignacio Rodriguez de Leon <euribates@gmail.com>
2018-11-11 12:08:49 +0000

    Lista de espera

--- events/forms.py
+++ events/forms.py
@@ -1,4 +1,12 @@
 from django import forms
 
+
 class EmailForm(forms.Form):
     email = forms.EmailField(label='Tu email', max_length=192)
+
+
+class WaitingListForm(forms.Form):
+    email = forms.EmailField(label='Tu email', max_length=192)
+    name = forms.CharField(label='Nombre', max_length=256)
+    surname = forms.CharField(label='Apellidos', max_length=256)
+    phone = forms.CharField(label='Teléfono', max_length=32)
