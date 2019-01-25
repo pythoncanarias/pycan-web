01351f7c2e958b1b4547d96310667c54c967f438
Juan Ignacio Rodriguez de Leon <euribates@gmail.com>
2019-01-25 17:38:11 +0000

    Models Quote and Author, initial data

---
+++ quotes/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=120)
+    surname = models.CharField(max_length=230)
+    url = models.URLField(blank=True)
+    photo = models.ImageField(upload_to='quotes/author/%Y/', blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return '{} {}'.format(self.name, self.surname)
+
+
+class Quote(models.Model):
+    text = models.CharField(max_length=580)
+    author = models.ForeignKey(Author, on_delete=models.PROTECT)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return '{}\n--\n{}'.format(self.text, self.author)
