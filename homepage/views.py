1ab0dc268efe26022dceb23be1916fbe0eff7c79
Sergio Delgado Quintero <sdelquin@gmail.com>
2019-02-10 12:59:12 +0000

    Add quote to homepage

--- homepage/views.py
+++ homepage/views.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 
 from django.shortcuts import render
+
 from events.models import Event
+from quotes.models import Quote
 
 
 def homepage(request):
     return render(request, 'homepage/index.html', {
         'active_events': Event.objects.all().filter(active=True).count(),
+        'quote': Quote.get_random_quote()
     })
