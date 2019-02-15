8fea4c73b58feca8551b6ae0ec0ab1f224e07ddf
Sergio Delgado Quintero <sdelquin@gmail.com>
2019-02-15 16:23:20 +0000

    Issue#176 (#177)
    * Change colors on header and footer
    
    * Add custom templatetag for checking active menuitem
    
    * Fix colors on main menu

--- commons/templatetags/utils.py
+++ commons/templatetags/utils.py
@@ -1,5 +1,8 @@
+import re
+
 from django import template
 from django.utils.safestring import mark_safe
+from django.urls import reverse
 
 from commons import filters
 
@@ -16,3 +19,14 @@ register.filter('get_key', filters.get_key)
 register.filter('get_asset_key', filters.get_asset_key)
 register.filter('as_markdown', compose2(mark_safe, filters.as_markdown))
 register.filter('sum_float', filters.sum_float)
+
+
+@register.simple_tag(takes_context=True)
+def is_active(context, named_url):
+    """Check if the current url is "under" named_url argument"""
+    path = context['request'].path
+    url = '^' + reverse(named_url)
+    if re.search(url, path):
+        return 'is-active'
+    else:
+        return ''
