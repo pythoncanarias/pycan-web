18b7a91714d63cfcf86649075a779e7a6957e176
Sergio Delgado Quintero <sdelquin@gmail.com>
2019-07-26 20:31:00 +0100

    Add SumUp as payment method

--- tickets/constants.py
+++ tickets/constants.py
@@ -2,8 +2,10 @@ class PAYMENT_METHOD:
     STRIPE = 1
     BANK_TRANSFERENCE = 2
     PAYPAL = 3
+    SUMUP = 4
     CHOICES = (
         (STRIPE, 'Stripe (default)'),
         (BANK_TRANSFERENCE, 'Bank transference'),
         (PAYPAL, 'PayPal'),
+        (SUMUP, 'SumUp'),
     )
