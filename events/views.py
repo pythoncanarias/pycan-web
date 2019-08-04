60c334939c08a7fd4e414d8d98f719b6c4ed9a14
Sergio Delgado Quintero <sdelquin@delorean.local>
2019-08-04 10:47:07 +0100

    Add past events to last section of event page

--- events/views.py
+++ events/views.py
@@ -31,8 +31,11 @@ def index(request):
 
 def detail_event(request, slug):
     event = Event.get_by_slug(slug)
+    past_events = Event.objects.filter(
+        active=False).order_by('-start_date')[:3]
     return render(request, 'events/event.html', {
         'event': event,
+        'past_events': past_events
     })
 
 
