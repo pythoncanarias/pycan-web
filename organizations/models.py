9f9d9b5ad04a78ef5b7f5d6bdddf20a726c973f9
Héctor Alvarez <monobot.soft@gmail.com>
2019-10-13 19:11:42 +0200

    fix #246 (#248)

--- organizations/models.py
+++ organizations/models.py
@@ -24,10 +24,7 @@ class Organization(models.Model):
         return self.name
 
     def joint_organizations(self):
-        return [
-            m.organization for m in self.joint_memberships.
-            order_by('-amount', 'order', 'organization__name')
-        ]
+        return [m.organization for m in self.joint_memberships.order_by('-amount', 'order', 'organization__name')]
 
     @property
     def paypal_me(self):
@@ -37,16 +34,10 @@ class Organization(models.Model):
 class OrganizationRole(models.Model):
     # Sponsor, Collaborator, Organizer, ...
     name = models.CharField(max_length=256)
-    order = models.PositiveIntegerField(
-        choices=PRIORITY.CHOICES,
-        default=PRIORITY.MEDIUM
-    )
+    order = models.PositiveIntegerField(choices=PRIORITY.CHOICES, default=PRIORITY.MEDIUM)
     code = models.CharField(max_length=32, unique=True)
     description = models.TextField(blank=True)
-    logo = models.FileField(
-        upload_to='organizations/organization_role/',
-        blank=True
-    )
+    logo = models.FileField(upload_to='organizations/organization_role/', blank=True)
     display_name = models.CharField(max_length=256, blank=True)
 
     def __str__(self):
@@ -56,16 +47,9 @@ class OrganizationRole(models.Model):
 class OrganizationCategory(models.Model):
     # Jade sponsor, Zafiro sponsor, Diamante sponsor, organizer, ...
     name = models.CharField(max_length=256)
-    order = models.PositiveIntegerField(
-        choices=PRIORITY.CHOICES,
-        default=PRIORITY.MEDIUM
-    )
+    order = models.PositiveIntegerField(choices=PRIORITY.CHOICES, default=PRIORITY.MEDIUM)
     code = models.CharField(max_length=32, unique=True)
-    role = models.ForeignKey(
-        OrganizationRole,
-        on_delete=models.PROTECT,
-        related_name='organization_categories'
-    )
+    role = models.ForeignKey(OrganizationRole, on_delete=models.PROTECT, related_name='organization_categories')
     description = models.TextField(blank=True)
     display_name = models.CharField(max_length=256, blank=True)
 
@@ -76,8 +60,7 @@ class OrganizationCategory(models.Model):
         verbose_name_plural = "organization categories"
 
     def organizations(self, event=None, exclude_joint_organizations=True):
-        memberships = self.memberships.order_by(
-            '-amount', 'order', 'organization__name')
+        memberships = self.memberships.order_by('-amount', 'order', 'organization__name')
         if event:
             memberships = memberships.filter(event=event)
         if exclude_joint_organizations:
@@ -86,30 +69,14 @@ class OrganizationCategory(models.Model):
 
 
 class Membership(models.Model):
-    event = models.ForeignKey(
-        'events.Event',
-        on_delete=models.PROTECT,
-        related_name='memberships'
-    )
-    organization = models.ForeignKey(
-        Organization,
-        on_delete=models.PROTECT,
-        related_name='memberships'
-    )
-    category = models.ForeignKey(
-        OrganizationCategory,
-        on_delete=models.PROTECT,
-        related_name='memberships'
-    )
+    event = models.ForeignKey('events.Event', on_delete=models.PROTECT, related_name='memberships')
+    organization = models.ForeignKey(Organization, on_delete=models.PROTECT, related_name='memberships')
+    category = models.ForeignKey(OrganizationCategory, on_delete=models.PROTECT, related_name='memberships')
     amount = models.DecimalField(max_digits=9, decimal_places=2, default=0)
     description = models.TextField(blank=True)
-    order = models.PositiveIntegerField(
-        choices=PRIORITY.CHOICES,
-        default=PRIORITY.MEDIUM
-    )
+    order = models.PositiveIntegerField(choices=PRIORITY.CHOICES, default=PRIORITY.MEDIUM)
     management_email = models.EmailField(
-        blank=True,
-        help_text='Management email of the organization used during the event'
+        blank=True, help_text='Management email of the organization used during the event'
     )
     joint_organization = models.ForeignKey(
         Organization,
