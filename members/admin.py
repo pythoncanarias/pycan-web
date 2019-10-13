10ca8c9274752e596b170c96197e3874225a3952
monobot <monobot.soft@gmail.com>
2019-10-13 10:01:43 +0200

    fix #207

--- members/admin.py
+++ members/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from members.models import Member
+from members.models import Member, Position
 
 
 @admin.register(Member)
 class MemberAdmin(admin.ModelAdmin):
     raw_id_fields = ['user']
+
+
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    pass
--- members/models.py
+++ members/models.py
@@ -4,6 +4,17 @@ from django.contrib.auth.models import User
 from django.db import models
 from pytz import utc
 
+POSITION_CHOICES = (
+    (0, 'Presidente'),
+    (0, 'Vicepresidente'),
+    (1, 'Secretario'),
+    (2, 'Tesorero'),
+    (3, 'Vocal 1'),
+    (4, 'Vocal 2'),
+    (5, 'Vocal 3'),
+    (5, 'Vocal 4'),
+)
+
 
 class Member(models.Model):
     user = models.OneToOneField(User, on_delete=models.PROTECT)
@@ -21,5 +32,21 @@ class Member(models.Model):
         return datetime.now(tz=utc) < self.member_until
 
     def __str__(self):
-        return (f'{self.user.first_name} {self.user.last_name} '
-                'member until {self.member_until}')
+        return (f'{self.user.first_name} {self.user.last_name} ' 'member until {self.member_until}')
+
+
+class Position(models.Model):
+    position = models.IntegerField(choices=POSITION_CHOICES)
+    member = models.ForeignKey(Member, on_delete=models.PROTECT)
+
+    since = models.DateField(auto_now_add=True)
+    until = models.DateField(blank=True, null=True)
+
+    active = models.BooleanField(default=True)
+
+    def save(self, *args, **kwargs):
+        created = not self.id
+        super().save(*args, **kwargs)
+
+        if created and self.active:
+            Position.objects.filter(active=True, position=self.position).update(until=self.since, active=False)
