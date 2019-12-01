86a0e0c12b75a75d4f54f248ce787f44c775556d
Sergio Delgado Quintero <sdelquin@gmail.com>
2019-12-01 13:22:04 +0000

    Fix membership dates

--- members/models.py
+++ members/models.py
@@ -1,32 +1,17 @@
-from datetime import datetime
-
 from django.contrib.auth.models import User
 from django.db import models
-from pytz import utc
 
-from .constants import (MEMBER_POSITION, FEE_PAYMENT_TYPE, FEE_AMOUNT,
-                        MEMBER_CATEGORY)
+from .constants import (FEE_AMOUNT, FEE_PAYMENT_TYPE, MEMBER_CATEGORY,
+                        MEMBER_POSITION)
 
 
 class Member(models.Model):
     user = models.OneToOneField(User, on_delete=models.PROTECT)
-
-    created_on = models.DateTimeField(auto_now_add=True)
-    member_until = models.DateTimeField(auto_now_add=True)
-
     address = models.CharField(max_length=100, blank=True)
     rest_address = models.CharField(max_length=100, blank=True)
     po_box = models.CharField(max_length=10, blank=True)
     city = models.CharField(max_length=20, blank=True)
     phone = models.CharField(max_length=20, blank=True)
-
-    @property
-    def active(self):
-        return datetime.now(tz=utc) < self.member_until
-
-    def __str__(self):
-        return (f'{self.user.first_name} {self.user.last_name} '
-                'member until {self.member_until}')
 
 
 class Position(models.Model):
