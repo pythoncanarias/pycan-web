4108bf633ae5fa238395085ead6a7f7741d294e6
Sergio Delgado Quintero <sdelquin@gmail.com>
2020-05-23 16:27:46 +0100

    Add jobs app to be handled by gulp

--- gulp/config.js
+++ gulp/config.js
@@ -1,18 +1,19 @@
-const APPS = ['commons', 'events', 'homepage', 'about', 'legal', 'members']
+const APPS = [
+  "commons",
+  "events",
+  "homepage",
+  "about",
+  "legal",
+  "members",
+  "jobs",
+];
 
 const LIBS = {
   CSS: {
-    commons: [
-      'node_modules/@fortawesome/fontawesome-free/css/all.css',
-    ],
-    homepage: [
-      'homepage/static/homepage/css/vendor.scss',
-    ]
+    commons: ["node_modules/@fortawesome/fontawesome-free/css/all.css"],
+    homepage: ["homepage/static/homepage/css/vendor.scss"],
   },
-  JS: {}
-}
+  JS: {},
+};
 
-export {
-  APPS,
-  LIBS
-}
+export { APPS, LIBS };
