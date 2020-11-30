739c3a62f49ec7ced9089d212d0caf5e6e46fe05
Juan Ignacio Rodriguez de Leon <euribates@gmail.com>
2020-11-30 19:00:33 +0000

    New views: homepage, membership, password change

--- members/urls.py
+++ members/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
+
 from . import views
+from . import forms
 
 app_name = 'members'
 
 urlpatterns = [
+    path('', views.homepage, name='homepage'),
     path('join/', views.join, name='join'),
     path("profile/", views.profile, name="profile"),
+    path("membership/", views.view_membership, name="membership"),
+    path("password/change/", views.password_change, name="password_change"),
     path("login/", views.member_login, name="login"),
     path("logout/", views.member_logout, name="logout"),
 ]
