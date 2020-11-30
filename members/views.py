35c8546fc80ca388974f3c0725df8dc7f97b97f6
Juan Ignacio Rodriguez de Leon <euribates@gmail.com>
2020-11-30 19:01:06 +0000

    New views: homepage, membership, password change

--- members/views.py
+++ members/views.py
@@ -1,16 +1,26 @@
 import logging
 
 from django.conf import settings
+from django.contrib import messages
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
 from organizations.models import Organization
 from . import forms
+from .menu import main_menu
 
 logger = logging.getLogger(__name__)
+
+
+def homepage(request):
+    if request.user.is_anonymous:
+        return join(request)
+    else:
+        return profile(request)
 
 
 def join(request):
@@ -27,10 +37,14 @@ def member_login(request: HttpRequest) -> HttpResponse:
         form = forms.LoginForm(request.POST)
         if form.is_valid():
             login(request, form.user)
-            return redirect(reverse("homepage"))
-    else:
+            return redirect(reverse("members:profile"))
+    elif request.method in set(['GET', 'HEAD']):
         username = request.GET.get('username', '')
         form = forms.LoginForm(initial={"username": username})
+    else:
+        raise PermissionDenied(
+            "Only GET, HEAD and POST HTTP verbs are valid here"
+            )
     return render(request, "members/login.html", {
         "title": "Acceder como socio",
         "form": form,
@@ -38,10 +52,10 @@ def member_login(request: HttpRequest) -> HttpResponse:
 
 
 def member_logout(request: HttpRequest) -> HttpResponse:
-    """Close the authenticated session ald log out of the system.
+    """Close the authenticated session and log out of the system.
     """
     logout(request)
-    return redirect(reverse("homepage"))
+    return redirect(reverse("members:homepage"))
 
 
 @login_required
@@ -52,4 +66,36 @@ def profile(request: HttpRequest) -> HttpResponse:
     return render(request, "members/profile.html", {
         "title": "Perfil socio {member.pk}: {member.full_name}",
         "member": member,
+        "menu": main_menu,
     })
+
+
+@login_required
+def view_membership(request: HttpRequest) -> HttpResponse:
+    member = request.user.member
+    return render(request, "members/membership.html", {
+        "title": "Socio {member.pk}: {member.full_name} - Pertenencia",
+        "member": member,
+        "membership": member.membership_set.all(),
+        "menu": main_menu,
+    })
+
+
+@login_required
+def password_change(request):
+    if request.method == 'GET':
+        form = forms.PasswordChangeForm(user=request.user)
+    elif request.method == 'POST':
+        form = forms.PasswordChangeForm(request.POST, user=request.user)
+        if form.is_valid():
+            form.save(request)
+            return redirect(reverse('members:profile'))
+        else:
+            messages.warning(request, "El formulario tiene errores")
+    else:
+        raise PermissionDenied("Only GET and POST HTTP verbs are valid here")
+    return render(request, "members/password-change.html", {
+        "title": "Cambio de contraseña",
+        "form": form,
+        "menu": main_menu,
+        })
