09b9d788b10c293795b2faf7c15b188ddb0942e4
Sergio Delgado Quintero <sdelquin@gmail.com>
2021-06-12 09:07:42 +0000

    Avoid members login if user is already authenticated

--- apps/members/views.py
+++ apps/members/views.py
@@ -5,14 +5,14 @@ from django.contrib import messages
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import PermissionDenied
-from django.http import HttpResponse, HttpRequest
-from django.shortcuts import render, redirect
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import redirect, render
 from django.urls import reverse
 
 from apps.organizations.models import Organization
+
 from . import forms
 from .menu import main_menu
-
 
 logger = logging.getLogger(__name__)
 
@@ -26,14 +26,17 @@ def homepage(request):
 
 def join(request):
     pythoncanarias = Organization.objects.get(
-        name__istartswith=settings.ORGANIZATION_NAME)
-    return render(request, 'members/join.html',
-                  {'pythoncanarias': pythoncanarias})
+        name__istartswith=settings.ORGANIZATION_NAME
+    )
+    return render(
+        request, 'members/join.html', {'pythoncanarias': pythoncanarias}
+    )
 
 
 def member_login(request: HttpRequest) -> HttpResponse:
-    """Allows a user to identify himself/herself with the system.
-    """
+    """Allows a user to identify himself/herself with the system."""
+    if request.user.is_authenticated:
+        return redirect(reverse('members:profile'))
     if request.method == 'POST':
         form = forms.LoginForm(request.POST)
         if form.is_valid():
@@ -45,41 +48,51 @@ def member_login(request: HttpRequest) -> HttpResponse:
     else:
         raise PermissionDenied(
             "Only GET, HEAD and POST HTTP verbs are valid here"
-            )
-    return render(request, "members/login.html", {
-        "title": "Acceder como socio",
-        "form": form,
-    })
+        )
+    return render(
+        request,
+        "members/login.html",
+        {
+            "title": "Acceder como socio",
+            "form": form,
+        },
+    )
 
 
 def member_logout(request: HttpRequest) -> HttpResponse:
-    """Close the authenticated session and log out of the system.
-    """
+    """Close the authenticated session and log out of the system."""
     logout(request)
     return redirect(reverse("members:homepage"))
 
 
 @login_required
 def profile(request: HttpRequest) -> HttpResponse:
-    """Show user profile and member information.
-    """
+    """Show user profile and member information."""
     member = request.user.member
-    return render(request, "members/profile.html", {
-        "title": "Perfil socio {member.pk}: {member.full_name}",
-        "member": member,
-        "menu": main_menu,
-    })
+    return render(
+        request,
+        "members/profile.html",
+        {
+            "title": "Perfil socio {member.pk}: {member.full_name}",
+            "member": member,
+            "menu": main_menu,
+        },
+    )
 
 
 @login_required
 def view_membership(request: HttpRequest) -> HttpResponse:
     member = request.user.member
-    return render(request, "members/membership.html", {
-        "title": "Socio {member.pk}: {member.full_name} - Pertenencia",
-        "member": member,
-        "membership": member.membership_set.all(),
-        "menu": main_menu,
-    })
+    return render(
+        request,
+        "members/membership.html",
+        {
+            "title": "Socio {member.pk}: {member.full_name} - Pertenencia",
+            "member": member,
+            "membership": member.membership_set.all(),
+            "menu": main_menu,
+        },
+    )
 
 
 @login_required
@@ -95,8 +108,12 @@ def password_change(request):
             messages.warning(request, "El formulario tiene errores")
     else:
         raise PermissionDenied("Only GET and POST HTTP verbs are valid here")
-    return render(request, "members/password-change.html", {
-        "title": "Cambio de contraseña",
-        "form": form,
-        "menu": main_menu,
-        })
+    return render(
+        request,
+        "members/password-change.html",
+        {
+            "title": "Cambio de contraseña",
+            "form": form,
+            "menu": main_menu,
+        },
+    )
