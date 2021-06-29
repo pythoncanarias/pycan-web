e0c10458b91bd0d27e2bc31382856fb5478cf469
Juan Ignacio Rodriguez de Leon <euribates@gmail.com>
2021-06-29 22:26:07 +0100

    New url for address change

--- apps/members/urls.py
+++ apps/members/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path("profile/", views.profile, name="profile"),
     path("membership/", views.view_membership, name="membership"),
     path("password/change/", views.password_change, name="password_change"),
+    path("address/change/", views.ChangeAddress.as_view(), name="address_change"),
     path("login/", views.member_login, name="login"),
     path("logout/", views.member_logout, name="logout"),
 ]
