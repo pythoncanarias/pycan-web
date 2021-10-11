75bbc4da672646959a833a03fece5fc175e18abe
AmandaKhol <amanda.camsua@gmail.com>
2021-10-11 15:13:03 +0200

    feat: add new links in the nav

--- apps/commons/static/commons/js/general/index.js
+++ apps/commons/static/commons/js/general/index.js
@@ -1,9 +1,11 @@
 import NavbarBurger from './navbar-burger'
 import Notifications from './notifications'
+import Collapsable from './collapsable'
 
 export default {
   init: () => {
     NavbarBurger.init()
     Notifications.init()
-  }
+    Collapsable.init()
+  },
 }
