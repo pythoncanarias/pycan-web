144f033917d04c095f38ee08192ec961ed38128b
Manuel Heredia <manolo.heredia@gmail.com>
2021-10-25 23:15:31 +0200

    50char->|             72char->| Add FAQ link to menu and fix bug CSS not showing
    * Fix :bug: not showing css in faq app:
      * move css for faq to the proper path in
        `/apps/faq/static/faq/css/main.scss`
      * correct reference to css in `/apps/faq/templates/faq/base.html`
      * set `{% block body_class %}` to `faq-index` in
        `/apps/faq/templates/faq/faq_list.html`
    
    * add FAQ link in the main menu:
      * add faq to menu in `apps/commons/templates/header.html`

--- apps/faq/admin.py
+++ apps/faq/admin.py
@@ -4,19 +4,3 @@ from django.contrib import admin
 from .models import FAQItem
 
 admin.site.register(FAQItem)
-
-"""
-# una referencia de pycan:
-
-from . import models
-
-
-@admin.register(models.Ally)
-class AllyAdmin(admin.ModelAdmin):
-    list_display = (
-        'name',
-        'url',
-        'twitter',
-        'email',
-    )
-"""
