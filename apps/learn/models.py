9e81d335f12d618d8bc08f630ae7e522b7d9d062
Sergio Delgado Quintero <sdelquin@gmail.com>
2022-06-02 18:32:47 +0000

    Add admin classes for learn app

--- apps/learn/models.py
+++ apps/learn/models.py
@@ -5,9 +5,15 @@ class Label(models.Model):
     name = models.CharField(max_length=120)
     slug = models.SlugField(max_length=120, unique=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Resource(models.Model):
     name = models.CharField(max_length=120)
     slug = models.SlugField(max_length=120, unique=True)
     url = models.URLField(max_length=240)
     labels = models.ManyToManyField(to=Label, related_name='resources')
+
+    def __str__(self):
+        return self.name
