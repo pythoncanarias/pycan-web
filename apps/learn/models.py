0b668c19b4c7ae9d0d8f7df86f522a0211f48e54
Sergio Delgado Quintero <sdelquin@gmail.com>
2022-06-02 18:15:25 +0000

    Add initial migration for learn app

--- apps/learn/models.py
+++ apps/learn/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Label(models.Model):
+    name = models.CharField(max_length=120)
+    slug = models.SlugField(max_length=120, unique=True)
+
+
+class Resource(models.Model):
+    name = models.CharField(max_length=120)
+    slug = models.SlugField(max_length=120, unique=True)
+    url = models.URLField(max_length=240)
+    labels = models.ManyToManyField(to=Label, related_name='resources')
