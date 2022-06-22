2c3024b52dc2c2abb348be5c81a097b144293f17
Sergio Delgado Quintero <sdelquin@gmail.com>
2022-06-22 21:44:52 +0000

    Add custom styles to labels on learning resources

--- gulp/config.js
+++ gulp/config.js
@@ -1,35 +1,32 @@
-import path from 'path';
+import path from 'path'
 
-
-function getNodeModulesPathFrom(module_name)
-{
-  let module_path = require.resolve(module_name);
-  while(module_path !== '/' && path.basename(module_path) !== "node_modules")
-  {
-    module_path = path.dirname(module_path);
+function getNodeModulesPathFrom(module_name) {
+  let module_path = require.resolve(module_name)
+  while (module_path !== '/' && path.basename(module_path) !== 'node_modules') {
+    module_path = path.dirname(module_path)
   }
-  return module_path;
+  return module_path
 }
 
-
 const APPS = [
-  "commons",
-  "events",
-  "homepage",
-  "about",
-  "legal",
-  "members",
-  "jobs",
-];
+  'commons',
+  'events',
+  'homepage',
+  'about',
+  'legal',
+  'members',
+  'jobs',
+  'learn',
+]
 
-const NODE_MODULES_DIR = getNodeModulesPathFrom('@fortawesome/fontawesome-free');
+const NODE_MODULES_DIR = getNodeModulesPathFrom('@fortawesome/fontawesome-free')
 
 const LIBS = {
   CSS: {
-    commons: [NODE_MODULES_DIR + "/@fortawesome/fontawesome-free/css/all.css"],
-    homepage: ["apps/homepage/static/homepage/css/vendor.scss"],
+    commons: [NODE_MODULES_DIR + '/@fortawesome/fontawesome-free/css/all.css'],
+    homepage: ['apps/homepage/static/homepage/css/vendor.scss'],
   },
   JS: {},
-};
+}
 
-export { APPS, LIBS, NODE_MODULES_DIR };
+export { APPS, LIBS, NODE_MODULES_DIR }
