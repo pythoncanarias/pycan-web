7f96a6227a909d842580d8a9d296d6efda75ebf6
Juan Ignacio Rodriguez de Leon <jileon@parcan.es>
2025-02-25 14:52:25 +0000

    Add filter as_boolean

--- apps/commons/templatetags/utils.py
+++ apps/commons/templatetags/utils.py
@@ -24,6 +24,78 @@ register.filter('startswith', filters.startswith)
 register.filter('msgtag_to_bulmaclass', filters.msgtag_to_bulmaclass)
 register.filter('duration_in_minutes', filters.duration_in_minutes)
 
+#: Words to be considered like ``False``.
+NEGATIVE_VALUES = {
+    '0',
+    'F',
+    'FALSE',
+    'INACTIVE',
+    'N',
+    'NEVER',
+    'NO',
+    'NOT',
+    'NONE',
+    'OFF',
+    }
+
+@register.filter
+def as_boolean(value, options=None):
+    """
+    Returns a textual representation of a boolean in HTML.
+
+    Unlike Python, a non-empty string could still be treated as
+    ``False`` if is in a list of reserved words, like ``'N'``, ``'no'``,
+    ``'not'`` ``'False'``, and others.  This is useful for values
+    retrieved from configuration files, line ``.ini`` o ``.env``.
+
+    If ``value`` is ``None``, returns ``N/A`` (Does Not Apply).
+
+    .. note::
+
+        List of reserved words to be considered as negatives is in
+        contant ``NEGATIVE_VALUES``.
+
+    Use examples:
+
+        >>> assert as_boolean('False') == as_boolean(False)
+        >>> assert as_boolean('False', 'OK,ERROR') == 'ERROR'
+        >>> assert as_boolean(True, 'OK,ERROR') == 'OK'
+
+    Params:
+
+        value (Any): Value to be interpreted as a boolean. Could
+          be any kind, and truey/falsey rules from python still
+          apply, with the exception of a reduced set of words, as
+          explained.
+
+        options (str): Optional. If used, must be a string that follows
+          one of this formats:
+
+            * ``<Yes>``
+            * ``<Yes>,<Not>``
+            * ``<Yes>,<Not>,<Does not apply>``
+
+    """
+    _if_true = '<strong class="boolean yes" style="color: #18442D">\u2705 Sí</strong>'
+    _if_false = '<strong class="boolean no" style="color: #321A22">\u274C No</strong>'
+    _if_does_not_apply = '<strong class="boolean" style="color: #9E9E9E">N/A</strong>'
+    if options:
+        if ',' not in options:
+            _if_true = options
+        else:
+            _if_true, _rest = options.split(',', 1)
+            if ',' in _rest:
+                _if_false, _if_does_not_apply = _resto.split(',', 1)
+            else:
+                _if_false = _if_does_not_apply = _rest
+    if value is None:
+        return _if_does_not_apply
+    if isinstance(value, str):
+        if value.upper() in NEGATIVE_VALUES:
+            return _if_false
+    return mark_safe(_if_true if bool(value) else _if_false)
+
+
 
 @register.simple_tag(takes_context=True)
 def is_active(context, named_url):
