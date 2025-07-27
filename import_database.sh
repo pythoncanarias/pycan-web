13e51a07e7cca021483e8d35a5ac107de5d60dd2
Juan Ignacio Rodriguez de Leon <euribates@gmail.com>
2025-07-28 00:13:02 +0100

    Script de importacion

---
+++ import_database.sh
@@ -0,0 +1,24 @@
+#/bin/bash
+
+# scp jileon@www.pythoncanarias.es:web/exported.zip .
+# unzip exported.zip
+
+./manage.py loaddata exported/about.json
+./manage.py loaddata exported/organizations.json
+./manage.py loaddata exported/quotes.json
+./manage.py loaddata exported/locations.json
+./manage.py loaddata exported/learn.json
+
+./manage.py loaddata exported/events.json
+./manage.py loaddata exported/schedule.json
+./manage.py loaddata exported/speakers.json
+./manage.py loaddata exported/tickets.json
+./manage.py loaddata exported/invoices.json
+
+./manage.py loaddata exported/jobs.json
+
+./manage.py loaddata exported/members.json
+./manage.py loaddata exported/notices.json
+
+# ./manage.py loaddata exported/certificates.json
+
