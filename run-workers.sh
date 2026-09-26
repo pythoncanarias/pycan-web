76d72f9f9c4923b5dc002f306f5b0d82a4d6d2af
Juan Ignacio Rodríguez de León <euribates@gmail.com>
2026-09-26 15:42:43 +0000

    Paso a explotación

--- run-workers.sh
+++ run-workers.sh
@@ -2,4 +2,4 @@
 
 cd /home/jileon/apps/pycan-web
 source .venv/bin/activate
-uv run python manage.py rqworker --with-scheduler high default low
+python manage.py rqworker --with-scheduler high default low
